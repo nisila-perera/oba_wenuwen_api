c22b0eb0ff6516ba1302e88a3b9d6cb34b87f0b1
tikka_2004 <nisilainsitha1@gmail.com>
2021-05-31 18:30:15 +0530

    final1

--- view.php
+++ view.php
@@ -86,7 +86,7 @@ and open the template in the editor.
             url: "https://api.obawenuwenapi.ictforlife.org/api/person/get-geo-data",
             headers: {
                 'app-token': '$*P?vm!QT?_sX=hv+jAsFgxmc2EFB!',
-                'user-token': '65f1e5e0c97ecb377b024bf81955c06a'
+                'user-token': 'cf6ca479090a48dbd30bb1cf77328f86'
             },
             type: "GET",
             contentType: false,
